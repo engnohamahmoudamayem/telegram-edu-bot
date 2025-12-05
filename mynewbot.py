c2c623658703f51636eb703352d395f82c981965
Noha Mahmoud <noha@example.com>
2025-12-05 16:18:12 +0300

    update

--- mynewbot.py
+++ mynewbot.py
@@ -42,7 +42,9 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 APP_URL = os.environ.get("APP_URL")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
-print("🔥 ADMIN_PASSWORD LOADED FROM ENV =", repr(ADMIN_PASSWORD))
+# FIX NEWLINE & SPACES
+ADMIN_PASSWORD = ADMIN_PASSWORD.strip()
+print("🔥 CLEAN ADMIN_PASSWORD =", repr(ADMIN_PASSWORD))
 
 if not BOT_TOKEN or not APP_URL:
     raise RuntimeError("BOT_TOKEN or APP_URL missing!")
